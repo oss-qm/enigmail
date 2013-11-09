4e4b015559a463b16c964b98fffd84bdd82ad7ce
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2013-11-08 19:23:52 -0500

    Imported Upstream version 1.6

--- configurehelper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-`dirname $0`/configure --with-system-libxul --disable-strip --disable-ogg --disable-wave --disable-dbus --disable-libnotify --disable-necko-wifi --disable-crashreporter --disable-webm --disable-libjpeg-turbo --disable-tests --disable-elf-hack --disable-webrtc "$@"
